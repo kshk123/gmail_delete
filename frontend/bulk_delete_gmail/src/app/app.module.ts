9f1beb01d7b1c441b2847f99e3d9eaa37b0e9085
Kaushik Basu <kshk123@gmail.com>
2023-05-04 17:42:27 +0200

    Some UI changes to confirm from user before trigerring deletion

--- frontend/bulk_delete_gmail/src/app/app.module.ts
+++ frontend/bulk_delete_gmail/src/app/app.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DeleteGmailFormComponent } from './delete-gmail-form/delete-gmail-form.component';
+import { DeleteGmailFormComponent, ConfirmationDialogComponent } from './delete-gmail-form/delete-gmail-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeleteGmailFormComponent
+    DeleteGmailFormComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/bulk_delete_gmail/src/app/delete-gmail-form/delete-gmail-form.component.ts
+++ frontend/bulk_delete_gmail/src/app/delete-gmail-form/delete-gmail-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, InjectionToken } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-gmail-form',
@@ -9,8 +10,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DeleteGmailFormComponent implements OnInit {
   form!: FormGroup;
+  serverResponse: string = '';
+
   
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, public dialog: MatDialog) { }
   
   ngOnInit(): void {
     // Build the form using FormBuilder
@@ -18,6 +21,12 @@ export class DeleteGmailFormComponent implements OnInit {
       email_id    : '',
       search_query: ''
     });
+  }
+
+  confirmDelete() {
+    if (confirm("Are you sure you want to delete the emails?")) {
+      this.onSubmit();
+    }
   }
 
   onSubmit(): void {
@@ -29,6 +38,7 @@ export class DeleteGmailFormComponent implements OnInit {
     this.http.post('http://127.0.0.1:5000/delete-gmails', { email, query }).subscribe(
       (response) => {
         // Handle successful response from server
+        this.serverResponse = response.toString();
         console.log(response);
       },
       (error) => {
@@ -37,4 +47,44 @@ export class DeleteGmailFormComponent implements OnInit {
       }
     );
   }
+
+  showConfirmation(): void {
+    // Get the user input values from the form
+    const email = this.form.value.email_id;
+    const query = this.form.value.search_query;
+
+    // Open a confirmation dialog box with the values of email_id and search_query
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '350px',
+      data: { email_id: email, search_query: query }
+    });
+
+    // When the user clicks the 'Yes' button, execute the script
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.onSubmit();
+      }
+    });
+  }
 }
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+    <h1 mat-dialog-title>Confirm Delete?</h1>
+    <div mat-dialog-content>
+      <p>Are you sure you want to delete emails with the following criteria?</p>
+      <p><strong>Email:</strong> {{ data.email_id }}</p>
+      <p><strong>Search Query:</strong> {{ data.search_query }}</p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="false">No</button>
+      <button mat-button [mat-dialog-close]="true">Yes</button>
+    </div>
+  `
+})
+export class ConfirmationDialogComponent {
+  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+}
+
+
